653a798f465c4b6c25abfce8811e1da07dd5a3e2
houahidi <houahidi@uni-consulting.fr>
2018-09-26 11:08:14 +0100

    add webapp itegration test

---
+++ library-parent/library-webapp/src/test/java/fr/uni/institute/library/web/tests/IndexITestCase.java
@@ -0,0 +1,34 @@
+package fr.uni.institute.library.web.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
+
+public class IndexITestCase {
+
+	@Test
+	public void test() {
+		final String ADDRESS = "http://localhost:8080/library-web/";
+		
+
+		 	try {
+					WebConversation webConversation = new WebConversation();
+					WebResponse menuResponse = webConversation.getResponse(ADDRESS);
+					assertEquals("UNI Institute - UNI Library - fr_FR", menuResponse.getTitle());
+				} catch (SAXException  e) {
+					fail("SAXException: " + e.getMessage());
+				} catch (IOException e) {
+					fail("IOException: " + e.getMessage());
+				}
+			
+	}
+
+}
